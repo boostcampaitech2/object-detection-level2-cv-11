27706e19079ce5d67f6702a5e569437f34f162c6
jinwoo <tjdwnsdud3@naver.com>
2021-09-29 10:49:07 +0000

    ADD cutmix

--- mmdetection/configs/_base_/jinwoo.py
+++ mmdetection/configs/_base_/jinwoo.py
@@ -9,7 +9,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=(1024, 1024),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -165,7 +165,7 @@ model = dict(
 
 load_from = "/opt/ml/detection/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
 
-# optimizer
+# Scheduler
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
@@ -177,7 +177,7 @@ lr_config = dict(
     step=[8, 11])
 runner = dict(type='EpochBasedRunner', max_epochs=48)
 
-work_dir = '/opt/ml/detection/mmdetection/work_dirs/younghoon'
+work_dir = '/opt/ml/detection/mmdetection/work_dirs/jinwoo'
 
 checkpoint_config = dict(max_keep_ckpts=3, interval=4)
 # yapf:disable
